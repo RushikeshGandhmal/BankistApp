9efd578c8b6619008f87e2a43738ea358f19db7b
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-05-25 17:50:01 +0530

    add load

--- script.js
+++ script.js
@@ -175,3 +175,21 @@ btnClose.addEventListener("click", (e) => {
   }
   inputCloseUsername.value = inputClosePin.value = "";
 });
+
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = +inputLoanAmount.value;
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});

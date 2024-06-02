ba1b00369291d0448e76c5a40f07e6e00963d875
Rushikesh Gandhmal <rushikeshgandhmal@gmail.com>
2024-06-02 18:25:20 +0530

    selecting, setting the style, attributes and classes

--- script.js
+++ script.js
@@ -46,3 +46,36 @@ document
     message.remove(); // new
     // message.parentElement.removeChild(message);
   });
+
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
+
+// document.documentElement.style.setProperty("--color-primary", "yellow");
+
+// Attributes
+const logo = document.querySelector(".nav__logo");
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = "Beautiful minimalist logo";
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute("designer"));
+logo.setAttribute("company", "Bankist");
+
+console.log(logo.src);
+console.log(logo.getAttribute("src"));
+
+const link = document.querySelector(".nav__link--btn");
+console.log(link.href);
+console.log(link.getAttribute("href"));
